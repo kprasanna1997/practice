3058b8819812d46ba2eccc24877b1f5437a515be
kprasanna1997 <kprasanna331@gmail.com>
2024-12-25 15:07:24 +0530

    Counter store integrated with counter component.

--- src/app/main/counter/counter.component.ts
+++ src/app/main/counter/counter.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { AppStore } from '../../stores/app-store';
+import { selectCount } from '../../stores/counter/conter.selector';
+import { decrement, increment, reset } from '../../stores/counter/counter.action';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, ButtonComponent],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
 
+  count$!: Observable<number>;
+
+  constructor(private store: Store<AppStore>) {
+    this.count$ = this.store.select(selectCount)
+  }
+
+  increment() {
+    this.store.dispatch(increment())
+  }
+
+  decrement() {
+    this.store.dispatch(decrement())
+  }
+
+  reset() {
+    this.store.dispatch(reset())
+  }
 }

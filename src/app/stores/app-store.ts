b1794cdfc6858a5b096e9cee08e2bb192b5df70b
kprasanna1997 <kprasanna331@gmail.com>
2025-01-06 23:43:22 +0530

    AppStore updated.

--- src/app/stores/app-store.ts
+++ src/app/stores/app-store.ts
@@ -1,5 +1,9 @@
+import { ProductCartState } from "./cart/cart.reducer";
 import { CounterState } from "./counter/counter.reducer";
+import { ProductsState } from "./products/products.reducer";
 
 export interface AppStore {
-    counter: CounterState
+    counter: CounterState,
+    cart: ProductCartState,
+    products: ProductsState
 }

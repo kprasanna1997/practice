0e76fd7bf9014243e0915e4c0280ca314994752a
kprasanna1997 <kprasanna331@gmail.com>
2025-01-06 23:40:07 +0530

    ProductsApiService created and getProducts api method integrated.

---
+++ src/app/main/services/products/products-api.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../../../interfaces/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>('https://fakestoreapi.com/products');
+  }
+}

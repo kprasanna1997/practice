f70d43045b1545adc8b6d0192baca7842c92ebcc
kprasanna1997 <kprasanna331@gmail.com>
2025-01-07 23:44:42 +0530

    Control block if, for and switch implemented.

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './child.component.html',
   styleUrl: './child.component.scss'
 })
 export class ChildComponent {
+
+  number1 = 100;
+  number2 = 200;
+
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  letter = 'a';
 
 }

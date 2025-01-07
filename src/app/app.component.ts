e034008abb6a409b90bd8194895a98a081e842db
kprasanna1997 <kprasanna331@gmail.com>
2025-01-07 23:22:01 +0530

    defer Control block integrated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'practice';
+
+  isDeferredBlock: WritableSignal<boolean> = signal(false);
+
 }
